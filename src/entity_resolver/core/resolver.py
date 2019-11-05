2f357be5d9e38a586d2a2c9ddcb6fcb6e6a75597
Yiming Huang <hym961004@gmail.com>
2019-11-05 08:34:55 +0800

    Complete attribute and relational similarity measures for nodes and clusters.

--- src/entity_resolver/core/resolver.py
+++ src/entity_resolver/core/resolver.py
@@ -1,59 +1,7 @@
 import itertools
 import collections
+import numpy as np
 from py_stringmatching.similarity_measure import soft_tfidf, jaro_winkler
-
-
-class SimFuncFactory:
-
-    @staticmethod
-    def produce_jaro_winkler(weight, corpus_list):
-        sim_func = jaro_winkler.JaroWinkler()
-        soft_tfidf = soft_tfidf.SoftTfIdf(corpus_list, sim_func)
-
-        def jaro_winkler(value1, value2):
-            return weight * soft_tfidf.get_raw_score(value1, value2)
-        return jaro_winkler
-
-    @staticmethod
-    def produce_common_neighbors(denominator):
-        def common_neighbors(neighbors1, neighbors2):
-            return len(set(neighbors1) & set(neighbors2)) / denominator
-        return common_neighbors
-
-    @staticmethod
-    def produce_common_neighbors_fr(denominator):
-        def common_neighbors_fr(neighbors1, neighbors2):
-            counter1 = collections.Counter(neighbors1)
-            counter2 = collections.Counter(neighbors2)
-            common_neighbors = 0
-            for node, count in counter1.items():
-                common_neighbors += min(count, counter2.get(node, 0))
-            return common_neighbors / denominator
-        return common_neighbors_fr
-
-    @staticmethod
-    def produce_jaccard_coef():
-        def jaccard_coef(neighbors1, neighbors2):
-            neighbors1_set = set(neighbors1)
-            neighbors2_set = set(neighbors2)
-            intersection = len(neighbors1_set & neighbors2_set)
-            union = len(neighbors1_set | neighbors2_set)
-            return intersection / union
-        return jaccard_coef
-
-    @staticmethod
-    def produce_jaccard_coef_fr():
-        def jaccard_coef_fr(neighbors1, neighbors2):
-            counter1 = collections.Counter(neighbors1)
-            counter2 = collections.Counter(neighbors2)
-            common_neighbors, total_neighbors = 0, 0
-            for node in set(counter1.keys()) | set(counter2.keys()):
-                count1 = counter1.get(node, 0)
-                count2 = counter2.get(node, 0)
-                common_neighbors += min(count1, count2)
-                total_neighbors += max(count1, count2)
-            return common_neighbors / total_neighbors
-        return jaccard_coef_fr
 
 
 class Resolver:
@@ -68,14 +16,16 @@ class Resolver:
         self.rel_strategy = rel_strategy
         self._sim_func_producers = {
             'jaro_winkler': SimFuncFactory.produce_jaro_winkler,
-            'common_neighbors': SimFuncFactory.produce_common_neighbors,
-            'common_neighbors_fr': SimFuncFactory.produce_common_neighbors_fr,
             'jaccard_coef': SimFuncFactory.produce_jaccard_coef,
-            'jaccard_coef_fr': SimFuncFactory.produce_jaccard_coef
+            'jaccard_coef_fr': SimFuncFactory.produce_jaccard_coef_fr,
+            'adar_neighbor': SimFuncFactory.produce_adar_neighbor,
+            'adar_neighbor_fr': SimFuncFactory.produce_adar_neighbor_fr,
+            'adar_attr': SimFuncFactory.produce_adar_attr,
+            'adar_attr_fr': SimFuncFactory.produce_adar_attr_fr
         }
         self._default_strategies = {
             'text': 'jaro_winkler',
-            'relation': 'common_neighbors'
+            'relation': 'jaccard_coef'
         }
         pass
 
@@ -87,7 +37,7 @@ class Resolver:
         self._graph = graph
         self._clusters = {node.id: set([node]) for node in graph.nodes}
         self._inv_clusters = {node: node.id for node in graph.nodes}
-        parsed_result = self._parse_strategy(graph)
+        parsed_result = self._parse_strategy()
         self._attr_weights, self._attr_funcs, self._rel_func = parsed_result
         self._init_attr_sims()
         self._ambiguities = graph.get_ambiguity_adar()
@@ -110,7 +60,7 @@ class Resolver:
 
     def _parse_attr_strats(self, attr_vals, attr_weights):
         attr_sim_funcs = dict()
-        attr_types = self._graph.get_attr_types()
+        attr_types = self._graph.attr_types
         for name, attr_values in attr_vals.items():
             weight = attr_weights[name]
             attr_type = attr_types[name]
@@ -125,15 +75,17 @@ class Resolver:
 
     def _parse_rel_strat(self):
         if self.rel_strategy is None:
-            rel_strategy = 'relation'
+            rel_strategy = self._default_strategies['relation']
         else:
             rel_strategy = self.rel_strategy
         rel_sim_producer = self._sim_func_producers[rel_strategy]
-        num_nodes = len(self._graph.nodes)
-        return rel_sim_producer(num_nodes)
+        return rel_sim_producer()
 
     def _init_attr_sims(self):
-        attr_sim_matrix = dict()
+        def missing_factory():
+            return collections.defaultdict(float)
+
+        attr_sim_matrix = collections.defaultdict(missing_factory)
         for node1, node2 in itertools.combinations(self._graph.nodes, 2):
             similarity = self._calc_node_attr_sim(node1, node2)
             attr_sim_matrix[node1][node2] = similarity
@@ -164,13 +116,133 @@ class Resolver:
         return total_score / (len(nodes1)*len(nodes2))
 
     def _calc_rel_sim(self, cluster1, cluster2):
+        ambiguities = self._calc_cluster_amb()
         neighbors1 = self._get_cluster_neighbors(cluster1)
         neighbors2 = self._get_cluster_neighbors(cluster2)
-        rel_score = self._rel_func(neighbors1, neighbors2)
+        rel_score = self._rel_func(neighbors1, neighbors2, ambiguities)
         return rel_score
+
+    def _calc_cluster_amb(self):
+        neighbor_amb, attr_amb = dict(), dict()
+        ambiguities = self._ambiguities
+        for cluster, nodes in self._clusters.items():
+            num_neighbors = len(set(self._get_cluster_neighbors(cluster)))
+            # Add 1 to the denominator to avoid zero divider,
+            # but might not be appropriate
+            neighbor_amb[cluster] = 1 / (1+np.log(num_neighbors))
+            cluster_attr_amb = 0
+            for node in nodes:
+                # Tentatively use minimum ambiguity among all attributes
+                cluster_attr_amb += min(
+                    ambiguities[name][node.get_attr(name, get_raw=True)]
+                    for name in ambiguities
+                )
+            cluster_attr_amb /= len(nodes)
+            attr_amb[cluster] = 1 / cluster_attr_amb
+        return {'neighbor': neighbor_amb, 'attr': attr_amb}
 
     def _get_cluster_neighbors(self, cluster):
         nodes = self._clusters[cluster]
         neighbors_list = (self._graph.get_neighbors(node) for node in nodes)
         neighbor_nodes = itertools.chain(*neighbors_list)
         return [self._inv_clusters[node] for node in neighbor_nodes]
+
+
+class SimFuncFactory:
+
+    @classmethod
+    def produce_jaro_winkler(cls, weight, corpus_list):
+        sim_func = jaro_winkler.JaroWinkler().get_raw_score
+        soft_tfidf_obj = soft_tfidf.SoftTfIdf(corpus_list, sim_func)
+
+        def jaro_winkler_sim(value1, value2):
+            score1 = soft_tfidf_obj.get_raw_score(value1, value2)
+            score2 = soft_tfidf_obj.get_raw_score(value2, value1)
+            return weight * max(score1, score2)
+        return jaro_winkler_sim
+
+    @classmethod
+    def produce_jaccard_coef(cls):
+        def jaccard_coef(neighbors1, neighbors2, ambiguities):
+            set1 = set(neighbors1)
+            set2 = set(neighbors2)
+            intersect = len(set1 & set2)
+            union = len(set1 | set2)
+            return intersect / union
+        return jaccard_coef
+
+    @classmethod
+    def produce_jaccard_coef_fr(cls):
+        def jaccard_coef_fr(neighbors1, neighbors2, ambiguities):
+            counter1 = collections.Counter(neighbors1)
+            counter2 = collections.Counter(neighbors2)
+            union, intersect = cls._union_intersect_counter(
+                counter1, counter2,
+                lambda curr, count, key: curr + count
+            )
+            return intersect / union
+        return jaccard_coef_fr
+
+    @classmethod
+    def produce_adar_neighbor(cls):
+        def adar_neighbor(neighbors1, neighbors2, ambiguities):
+            ambiguities = ambiguities['neighbor']
+            set1 = set(neighbors1)
+            set2 = set(neighbors2)
+            union = set1 | set2
+            intersect = set1 & set2
+            union_amb = sum(ambiguities[cluster] for cluster in union)
+            intersect_amb = sum(ambiguities[cluster] for cluster in intersect)
+            return intersect_amb / union_amb
+        return adar_neighbor
+
+    @classmethod
+    def produce_adar_neighbor_fr(cls):
+        def adar_neighbor_fr(neighbors1, neighbors2, ambiguities):
+            ambiguities = ambiguities['neighbor']
+            counter1 = collections.Counter(neighbors1)
+            counter2 = collections.Counter(neighbors2)
+            union_amb, intersect_amb = cls._union_intersect_counter(
+                counter1, counter2,
+                lambda curr, count, key: curr + count*ambiguities[key]
+            )
+            return intersect_amb / union_amb
+        return adar_neighbor_fr
+
+    @classmethod
+    def produce_adar_attr(cls):
+        def adar_attr(neighbors1, neighbors2, ambiguities):
+            ambiguities = ambiguities['attr']
+            set1 = set(neighbors1)
+            set2 = set(neighbors2)
+            union = set1 | set2
+            intersect = set1 & set2
+            union_amb = sum(ambiguities[cluster] for cluster in union)
+            intersect_amb = sum(ambiguities[cluster] for cluster in intersect)
+            return intersect_amb / union_amb
+        return adar_attr
+
+    @classmethod
+    def produce_adar_attr_fr(cls):
+        def adar_attr_fr(neighbors1, neighbors2, ambiguities):
+            ambiguities = ambiguities['attr']
+            counter1 = collections.Counter(neighbors1)
+            counter2 = collections.Counter(neighbors2)
+            union_amb, intersect_amb = cls._union_intersect_counter(
+                counter1, counter2,
+                lambda curr, count, key: curr + count*ambiguities[key]
+            )
+            return intersect_amb / union_amb
+        return adar_attr_fr
+
+    @staticmethod
+    def _union_intersect_counter(counter1, counter2, accumulator, initial=0):
+        union, intersect = initial, initial
+        for key in set(counter1.keys()) | set(counter2.keys()):
+            count1 = counter1.get(key, 0)
+            count2 = counter2.get(key, 0)
+            union_count = max(count1, count2)
+            intersect_count = min(count1, count2)
+            union = accumulator(union, union_count, key)
+            intersect = accumulator(intersect, intersect_count, key)
+        return union, intersect
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,8 @@
+import os
+import json
+import pytest
 from entity_resolver.core import Evaluator, Graph, Resolver
+from entity_resolver.parser import GraphParser
 
 
 class TestEvaluator:
@@ -17,6 +21,145 @@ class TestGraph:
 
 class TestResolver:
 
-    def test_dummy(self):
+    def _init_resolver(self, resolver, graph):
+        resolver._init_cache(graph)
+        nodes = {node.id: node for node in graph.nodes}
+        resolver._clusters = {
+            1: set([nodes[1], nodes[3]]),
+            2: set([nodes[2], nodes[5], nodes[7]]),
+            3: set([nodes[4]]),
+            4: set([nodes[6], nodes[8]])
+        }
+        resolver._inv_clusters = {
+            nodes[1]: 1,
+            nodes[2]: 2,
+            nodes[3]: 1,
+            nodes[4]: 3,
+            nodes[5]: 2,
+            nodes[6]: 4,
+            nodes[7]: 2,
+            nodes[8]: 4
+        }
+
+    @pytest.fixture
+    def simple_graph_suite(self):
+        parser = GraphParser({'text1': 'text', 'text2': 'text'})
+        tmp_path = 'simple_graph.json'
+        graph_dict = [
+            {
+                'node_id': 1,
+                'edge_id': 1,
+                'attr_dict': {'text1': 'a', 'text2': 'b'}
+            },
+            {
+                'node_id': 2,
+                'edge_id': 2,
+                'attr_dict': {'text1': 'a', 'text2': 'a'}
+            }
+        ]
+        with open(tmp_path, 'w') as f:
+            json.dump(graph_dict, f)
+
+        def cleanup():
+            os.remove(tmp_path)
+        return parser.parse(tmp_path), cleanup
+
+    @pytest.fixture
+    def complex_attr_graph_suite(self):
+        parser = GraphParser({'text': 'text'})
+        tmp_path = 'complex_attr_graph.json'
+        graph_dict = [
+            {'node_id': 1, 'edge_id': 1, 'attr_dict': {'text': 'a aa'}},
+            {'node_id': 2, 'edge_id': 2, 'attr_dict': {'text': 'b ab'}},
+            {'node_id': 3, 'edge_id': 3, 'attr_dict': {'text': 'a ba'}},
+            {'node_id': 4, 'edge_id': 4, 'attr_dict': {'text': 'b bb'}}
+        ]
+        with open(tmp_path, 'w') as f:
+            json.dump(graph_dict, f)
+
+        def cleanup():
+            os.remove(tmp_path)
+        return parser.parse(tmp_path), cleanup
+
+    @pytest.fixture
+    def complex_rel_graph_suite(self):
+        parser = GraphParser({'text': 'text'})
+        tmp_path = 'complex_rel_graph.json'
+        graph_dict = [
+            {'node_id': 1, 'edge_id': 'a', 'attr_dict': {'text': 'd'}},
+            {'node_id': 2, 'edge_id': 'a', 'attr_dict': {'text': 'b'}},
+            {'node_id': 3, 'edge_id': 'b', 'attr_dict': {'text': 'b'}},
+            {'node_id': 4, 'edge_id': 'b', 'attr_dict': {'text': 'c'}},
+            {'node_id': 5, 'edge_id': 'c', 'attr_dict': {'text': 'a'}},
+            {'node_id': 6, 'edge_id': 'c', 'attr_dict': {'text': 'd'}},
+            {'node_id': 7, 'edge_id': 'd', 'attr_dict': {'text': 'b'}},
+            {'node_id': 8, 'edge_id': 'd', 'attr_dict': {'text': 'a'}},
+        ]
+        with open(tmp_path, 'w') as f:
+            json.dump(graph_dict, f)
+
+        def cleanup():
+            os.remove(tmp_path)
+        return parser.parse(tmp_path), cleanup
+
+    def test_cache(self, simple_graph_suite):
+        graph, cleanup = simple_graph_suite
         resolver = Resolver()
-        assert 'resolve' in dir(resolver)
+        assert resolver.alpha == 0.5
+        assert len(resolver.attr_strategy) == 0
+        resolver._init_cache(graph)
+        for node_id, node_set in resolver._clusters.items():
+            node = list(node_set)[0]
+            assert node_id == node.id
+        for node, node_id in resolver._inv_clusters.items():
+            assert node.id == node_id
+        weights = resolver._attr_weights
+        assert weights['text1'] == 0.5
+        assert weights['text2'] == 0.5
+        node1 = list(resolver._clusters[1])[0]
+        node2 = list(resolver._clusters[2])[0]
+        matrix = resolver._attr_sim_matrix
+        assert matrix[node1][node2] == matrix[node2][node1] == 0.5
+        assert type(resolver._ambiguities) is dict
+        assert resolver._calc_rel_sim(1, 2) == 0
+        assert resolver._calc_similarity(1, 2) == 0.25
+        cleanup()
+
+    def test_attr_sim(self, complex_attr_graph_suite):
+        graph, cleanup = complex_attr_graph_suite
+        resolver = Resolver(attr_strategy={'text': 'jaro_winkler'})
+        resolver._init_cache(graph)
+        matrix = resolver._attr_sim_matrix
+        node1 = list(resolver._clusters[1])[0]
+        node2 = list(resolver._clusters[2])[0]
+        node3 = list(resolver._clusters[3])[0]
+        node4 = list(resolver._clusters[4])[0]
+        assert round(matrix[node1][node2], 2) == 0.90
+        assert round(matrix[node1][node3], 2) == 0.73
+        assert round(matrix[node1][node4], 2) == 0.00
+        assert round(matrix[node2][node3], 2) == 0.68
+        assert round(matrix[node2][node4], 2) == 0.73
+        assert round(matrix[node3][node4], 2) == 0.90
+        cleanup()
+
+    def test_rel_sim(self, complex_rel_graph_suite):
+        graph, cleanup = complex_rel_graph_suite
+        jaccard_coef_resolver = Resolver(rel_strategy='jaccard_coef')
+        jaccard_coef_fr_resolver = Resolver(rel_strategy='jaccard_coef_fr')
+        adar_neighbor_resolver = Resolver(rel_strategy='adar_neighbor')
+        adar_neighbor_fr_resolver = Resolver(rel_strategy='adar_neighbor_fr')
+        adar_attr_resolver = Resolver(rel_strategy='adar_attr')
+        adar_attr_fr_resolver = Resolver(rel_strategy='adar_attr_fr')
+        self._init_resolver(jaccard_coef_resolver, graph)
+        self._init_resolver(jaccard_coef_fr_resolver, graph)
+        self._init_resolver(adar_neighbor_resolver, graph)
+        self._init_resolver(adar_neighbor_fr_resolver, graph)
+        self._init_resolver(adar_attr_resolver, graph)
+        self._init_resolver(adar_attr_fr_resolver, graph)
+        assert jaccard_coef_resolver._calc_rel_sim(1, 2) == 0.5
+        assert jaccard_coef_fr_resolver._calc_rel_sim(1, 2) == 0.25
+        assert round(adar_neighbor_resolver._calc_rel_sim(1, 2), 2) == 0.45
+        assert round(adar_neighbor_fr_resolver._calc_rel_sim(1, 2), 2) == 0.23
+        assert round(adar_attr_resolver._calc_rel_sim(1, 2), 2) == 0.34
+        assert round(adar_attr_fr_resolver._calc_rel_sim(1, 2), 2) == 0.20
+        cleanup()
