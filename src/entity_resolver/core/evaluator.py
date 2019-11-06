7d4a32abeb8a911d2226b6d577a6d352816e98b4
Yiming Huang <hym961004@gmail.com>
2019-11-07 04:38:13 +0800

    Implement evaluator with two clustering metrics.

--- src/entity_resolver/core/evaluator.py
+++ src/entity_resolver/core/evaluator.py
@@ -1,7 +1,26 @@
+import functools
+from sklearn import metrics
+
+
 class Evaluator:
 
-    def __init__(self):
-        pass
+    strategy_funcs = {
+        'ami': functools.partial(
+            metrics.adjusted_mutual_info_score,
+            average_method='max'
+        ),
+        'v-measure': metrics.v_measure_score
+    }
 
-    def evaluate(self, resolved_mapping, ground_truth):
-        pass
+    def __init__(self, strategy='ami'):
+        self.strategy = self.strategy_funcs[strategy]
+
+    @staticmethod
+    def _reorder(mapping):
+        ordered_pairs = sorted(mapping.items(), key=lambda pair: pair[0])
+        return [pair[1] for pair in ordered_pairs]
+
+    def evaluate(self, ground_truth, resolved_mapping):
+        labels = self._reorder(ground_truth)
+        preds = self._reorder(resolved_mapping)
+        return self.strategy(labels, preds)
