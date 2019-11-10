ce2e2025d1e06fb2141fe395602297c3624b2f8f
Yiming Huang <hym961004@gmail.com>
2019-11-11 04:38:41 +0800

    Modify test since cluster initialization is no longer done in _init_cache

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 import os
 import json
+import collections
 import pytest
 from entity_resolver.core import Evaluator, Graph, Resolver
 from entity_resolver.parser import GraphParser
@@ -124,38 +125,29 @@ class TestResolver:
         assert resolver.alpha == 0.5
         assert len(resolver.attr_strategy) == 0
         resolver._init_cache(graph)
-        for node_id, node_set in resolver._clusters.items():
-            node = list(node_set)[0]
-            assert node_id == node.id
-        for node, node_id in resolver._inv_clusters.items():
-            assert node.id == node_id
         weights = resolver._attr_weights
         assert weights['text1'] == 0.5
         assert weights['text2'] == 0.5
-        node1 = list(resolver._clusters[1])[0]
-        node2 = list(resolver._clusters[2])[0]
-        matrix = resolver._attr_sim_matrix
-        assert matrix[node1][node2] == matrix[node2][node1] == 0.5
+        node1 = [node for node in graph.nodes if node.id == 1][0]
+        node2 = [node for node in graph.nodes if node.id == 2][0]
+        assert resolver._calc_node_attr_sim(node1, node2) == 0.5
         assert type(resolver._ambiguities) is dict
-        assert resolver._calc_rel_sim(1, 2) == 0
-        assert resolver._calc_similarity(1, 2) == 0.25
         cleanup()
 
     def test_attr_sim(self, complex_attr_graph_suite):
         graph, cleanup = complex_attr_graph_suite
         resolver = Resolver(attr_strategy={'text': 'jaro_winkler'})
         resolver._init_cache(graph)
-        matrix = resolver._attr_sim_matrix
-        node1 = list(resolver._clusters[1])[0]
-        node2 = list(resolver._clusters[2])[0]
-        node3 = list(resolver._clusters[3])[0]
-        node4 = list(resolver._clusters[4])[0]
-        assert round(matrix[node1][node2], 2) == 0.90
-        assert round(matrix[node1][node3], 2) == 0.73
-        assert round(matrix[node1][node4], 2) == 0.00
-        assert round(matrix[node2][node3], 2) == 0.68
-        assert round(matrix[node2][node4], 2) == 0.73
-        assert round(matrix[node3][node4], 2) == 0.90
+        node1 = [node for node in graph.nodes if node.id == 1][0]
+        node2 = [node for node in graph.nodes if node.id == 2][0]
+        node3 = [node for node in graph.nodes if node.id == 3][0]
+        node4 = [node for node in graph.nodes if node.id == 4][0]
+        assert round(resolver._calc_node_attr_sim(node1, node2), 2) == 0.90
+        assert round(resolver._calc_node_attr_sim(node1, node3), 2) == 0.73
+        assert round(resolver._calc_node_attr_sim(node1, node4), 2) == 0.00
+        assert round(resolver._calc_node_attr_sim(node2, node3), 2) == 0.68
+        assert round(resolver._calc_node_attr_sim(node2, node4), 2) == 0.73
+        assert round(resolver._calc_node_attr_sim(node3, node4), 2) == 0.90
         cleanup()
 
     def test_rel_sim(self, complex_rel_graph_suite):
