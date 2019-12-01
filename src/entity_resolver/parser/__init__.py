771bc75d56753466139acd02ff0cbc2d6d3c5e13
Yiming Huang <hym961004@gmail.com>
2019-11-30 23:06:13 -0500

    Add docstrings for ground_truth_parser module and the parser package.

--- src/entity_resolver/parser/__init__.py
+++ src/entity_resolver/parser/__init__.py
@@ -1,2 +1,11 @@
+""" A package consisting of data parsers.
+
+This package contains parser classes in
+:doc:`entity_resolver.parser.graph_parser` and
+doc:`entity_resolver.parser.ground_truth_parser`. It
+is created for import convenience similar to :doc:`entity_resolver`. For
+details, please follow the submodule links below to their documentation pages.
+"""
+
 from .graph_parser import GraphParser
 from .ground_truth_parser import GroundTruthParser
--- src/entity_resolver/parser/ground_truth_parser.py
+++ src/entity_resolver/parser/ground_truth_parser.py
@@ -1,12 +1,40 @@
+""" This module contains only one class to be used by the main module.
+
+Example:
+
+    >>> from entity_resolver.parser import GroundTruthParser
+    >>> parser = GroundTruthParser()
+    >>> ground_truth = parser.parse('ground truth.json')
+    >>> type(ground_truth)
+    <class 'collections.OrderedDict'>
+"""
+
 import json
 import time
 import collections
+from typing import Dict
 from ..core.utils import WithLogger
 
 
 class GroundTruthParser(WithLogger):
+    """ A parser object to parse ground truth data file.
 
-    def parse(self, data_path):
+    Currently this class only contains one method and no attributes, so it
+    is essentially serving as a dummy wrapper class of its parse method.
+    """
+    def parse(self, data_path: str) -> Dict:
+        """ Parse the ground truth data into an OrderedDict.
+
+        Args:
+            data_path: The path to the ground truth data. The data file has to
+                **strictly follow** the format as described in
+                :doc:`../quickstart`.
+
+        Returns:
+            A `collections.OrderedDict` mapping reference ids to ground truth
+            cluster. The dictionary is sorted (key-value pairs are inserted) in
+            ascending order of node id.
+        """
         start_time = time.time()
         ground_truth = collections.OrderedDict()
         with open(data_path, 'r') as f:
