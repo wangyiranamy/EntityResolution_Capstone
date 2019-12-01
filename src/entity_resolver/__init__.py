f2ebd093a19282a4de36dbacf933292a04ef5338
Yiming Huang <hym961004@gmail.com>
2019-11-30 21:48:35 -0500

    Add docstrings.

--- src/entity_resolver/__init__.py
+++ src/entity_resolver/__init__.py
@@ -1,1 +1,16 @@
+""" The interface package exposed to users.
+
+This package contains the single class in :doc:`entity_resolver.main` which
+encapsulates the whole collective entity resolution algorithm, and provides all
+functionalities to users for convenience. In short,
+
+    >>> from entity_resolver import EntityResolver
+
+is equivalent to
+
+    >>> from entity_resolver.main import EntityResolver
+
+For details, please refer to :doc:`entity_resolver.main`.
+"""
+
 from .main import EntityResolver
